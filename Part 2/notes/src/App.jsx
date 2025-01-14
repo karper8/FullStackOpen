ec348de1786b37bb490722ca7fbacfdec22a640d
Karthik P <karthik.tyson10@gmail.com>
2025-01-14 19:16:12 +0530

    added relative address

--- Part 2/notes/src/App.jsx
+++ Part 2/notes/src/App.jsx
@@ -16,7 +16,7 @@ function App(props) {
   useEffect(()=>{
     console.log('effect')
     noteService
-      .getAll('http://localhost:3001/api/notes')
+      .getAll('/api/notes')
       .then(initialNotes=>{
         console.log('promise fulfilled')
         setNotes(initialNotes)
@@ -50,7 +50,7 @@ function App(props) {
   }
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/api/notes/${id}`
+    const url = `/api/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = {...note, important: !note.important}
 
