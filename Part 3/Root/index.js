b06a8d26ae7808e807d45d2e340934c398a22e07
Karthik P <karthik.tyson10@gmail.com>
2025-05-02 23:54:20 +0530

    database

--- Part 3/Root/index.js
+++ Part 3/Root/index.js
@@ -21,23 +21,6 @@ morgan.token('body',function(req,res){return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-let notes = [
-    {
-        id:"1",
-        content:"HTML is easy",
-        important: true
-    },
-    {
-        id:"2",
-        content:"Browser can execute only javascript",
-        important: false
-    },
-    {
-        id:"3",
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
 
 app.get('/',(request,response) =>{
     response.send('<p>Hello World! </p>')
